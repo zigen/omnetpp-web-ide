e6c8ff3950cf0e77929c02b678f2ca046594a8d0
zigen <hrlclb@gmail.com>
2021-08-10 01:31:12 +0900

    add auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
